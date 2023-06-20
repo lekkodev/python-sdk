3ea550c088a0f56be74866aed9f43f5bcc79c726
Sam Marcellus <sam@brainsurprise.com>
2023-06-20 10:22:44 -0700

    improve coverage, fix bugs, upload to codecov (#9)
    * improve coverage, fix bugs, upload to codecov
    
    * fix ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,16 @@
 import concurrent.futures.thread
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 from unittest import mock
 
 import grpc
 import grpc_testing
 import pytest
 from google.protobuf.message import Message as ProtoMessage
+from grpc_testing import _channel  # noqa
 
+from lekko_client import helpers
 from lekko_client.gen.lekko.client.v1beta1.configuration_service_pb2 import DESCRIPTOR
 
 
@@ -25,7 +27,7 @@ def test_thread():
 
 
 @pytest.fixture
-def test_channel(test_thread):
+def test_channel_no_interceptor(test_thread):
     channel = grpc_testing.channel(DESCRIPTOR.services_by_name.values(), grpc_testing.strict_real_time())
     try:
         with mock.patch("lekko_client.client.get_grpc_channel", return_value=channel):
@@ -34,26 +36,53 @@ def test_channel(test_thread):
         channel.close()
 
 
+@pytest.fixture
+def test_channel(test_thread):
+    channel = grpc_testing.channel(DESCRIPTOR.services_by_name.values(), grpc_testing.strict_real_time())
+    try:
+        with mock.patch("grpc.insecure_channel", return_value=channel), mock.patch(
+            "grpc.secure_channel", return_value=channel
+        ):
+            yield channel
+    finally:
+        channel.close()
+
+
+class PatchedUnaryUnary(grpc_testing._channel._multi_callable.UnaryUnary):
+    # grpc_testing is very out of date and its fn sigs don't take wait_for_ready or compression
+    def with_call(self, request, timeout=None, metadata=None, credentials=None, wait_for_ready=None, compression=None):
+        return super().with_call(request, timeout, metadata, credentials)
+
+
+grpc_testing._channel._channel._multi_callable.UnaryUnary = PatchedUnaryUnary
+
+
 class MockServer:
     def __init__(self, channel, thread):
         self.channel = channel
         self.thread = thread
+        helpers._CHANNELS = {}
 
     @dataclass
     class MockRequestResponse:
         fn_name: str
         response: ProtoMessage
-        status_code = grpc.StatusCode.OK
+        status_code: grpc.StatusCode = grpc.StatusCode.OK
         error_text: str = ""
 
+    @dataclass
+    class CompletedRequest:
+        arg: ProtoMessage
+        metadata: Tuple[Tuple[str, str]]
+
     def mock_async_responses(self, mock_requests: List[MockRequestResponse]):
-        def server() -> List[ProtoMessage]:
+        def server() -> List[MockServer.CompletedRequest]:
             service = DESCRIPTOR.services_by_name["ConfigurationService"]
             incoming_requests = []
             for mock_request in mock_requests:
-                _, request, rpc = self.channel.take_unary_unary(service.methods_by_name[mock_request.fn_name])
+                metadata, request, rpc = self.channel.take_unary_unary(service.methods_by_name[mock_request.fn_name])
                 rpc.terminate(mock_request.response, [], mock_request.status_code, mock_request.error_text)
-                incoming_requests.append(request)
+                incoming_requests.append(MockServer.CompletedRequest(arg=request, metadata=metadata))
 
             return incoming_requests
 
@@ -64,3 +93,8 @@ class MockServer:
 @pytest.fixture
 def test_server(test_channel, test_thread):
     return MockServer(test_channel, test_thread)
+
+
+@pytest.fixture
+def test_server_no_interceptor(test_channel_no_interceptor, test_thread):
+    return MockServer(test_channel_no_interceptor, test_thread)
