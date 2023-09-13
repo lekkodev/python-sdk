3ffff9d018f18c74b65fb20a87df4cb2236619bb
Sam Marcellus <sam@brainsurprise.com>
2023-09-13 10:39:01 -0700

    add event tracking

--- lekko_client/clients/distribution_client.py
+++ lekko_client/clients/distribution_client.py
@@ -1,6 +1,9 @@
 import json
+import time
 from abc import abstractmethod
-from typing import Any, Dict, Optional, Type, TypeVar, Union
+from datetime import datetime
+from threading import Thread
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
 
 import grpc
 from google.protobuf import descriptor_pool as proto_descriptor_pool
@@ -9,25 +12,71 @@ from google.protobuf.any_pb2 import Any as ProtoAny
 from google.protobuf.json_format import MessageToJson
 from google.protobuf.message import Message as ProtoMessage
 from google.protobuf.struct_pb2 import Value
+from google.protobuf.timestamp_pb2 import Timestamp
 from google.protobuf.wrappers_pb2 import BoolValue, FloatValue, Int64Value, StringValue
 
 from lekko_client.clients.client import Client
-from lekko_client.evaluation.evaluation import evaluate
+from lekko_client.evaluation.evaluation import EvaluationResult, evaluate
+from lekko_client.evaluation.rules import ClientContext
 from lekko_client.exceptions import MismatchedProtoType
 from lekko_client.gen.lekko.backend.v1beta1.distribution_service_pb2 import (
+    ContextKey,
     DeregisterClientRequest,
+    FlagEvaluationEvent,
     GetRepositoryContentsResponse,
     RegisterClientRequest,
     RepositoryKey,
+    SendFlagEvaluationMetricsRequest,
 )
 from lekko_client.gen.lekko.backend.v1beta1.distribution_service_pb2_grpc import (
     DistributionServiceStub,
 )
+from lekko_client.gen.lekko.client.v1beta1.configuration_service_pb2 import (
+    Value as LekkoValue,
+)
 from lekko_client.helpers import convert_context, get_grpc_channel
+from lekko_client.models import FeatureData
 from lekko_client.stores.store import Store
 
 
 class CachedDistributionClient(Client):
+    class EventsBatcher(Thread):
+        def __init__(self, dist_client: DistributionServiceStub, session_key: str, upload_interval: int):
+            super().__init__()
+            self.daemon = True
+            self.dist_client = dist_client
+            self.upload_interval = upload_interval
+            self.session_key = session_key
+            self.events: List[FlagEvaluationEvent] = []
+            self._enabled = True
+
+        def stop(self):
+            self._enabled = False
+
+        def add_event(self, event: FlagEvaluationEvent):
+            self.events.append(event)
+
+        def run(self):
+            # TODO: Lock
+            while self._enabled:
+                if self.events:
+                    self.dist_client.SendFlagEvaluationMetrics(
+                        SendFlagEvaluationMetricsRequest(events=self.events, session_key=self.session_key)
+                    )
+                self.events = []
+                time.sleep(self.upload_interval / 1000)
+
+        @classmethod
+        def get_value_type(cls, val: LekkoValue) -> str:
+            return (val.WhichOneof("kind") or "").removesuffix("_value")
+
+        @classmethod
+        def get_context_keys(cls, context: Optional[ClientContext] = None) -> List[ContextKey]:
+            if not context:
+                return []
+
+            return [ContextKey(key=k, type=cls.get_value_type(v)) for k, v in context.items()]
+
     def __init__(
         self,
         uri: str,
@@ -52,8 +101,9 @@ class CachedDistributionClient(Client):
                 RegisterClientRequest(repo_key=self.repository, sidecar_version=__version__)
             )
             self.session_key = register_response.session_key
-            # if self.events_batcher:
-            # await self.events_batcher.init(self.session_key)
+            self.events_batcher = self.EventsBatcher(self._client, self.session_key, 15)
+            self.events_batcher.start()
+
         self.initialize()
 
     _TYPE_MAPPING: Dict[Type, Type[Union[BoolValue, Int64Value, StringValue, FloatValue]]] = {
@@ -78,9 +128,27 @@ class CachedDistributionClient(Client):
     def get_contents(self) -> Optional[GetRepositoryContentsResponse]:
         ...
 
+    def track(
+        self, namespace: str, feature_data: FeatureData, result: EvaluationResult, context: Optional[ClientContext]
+    ) -> None:
+        timestamp = Timestamp()
+        timestamp.FromDatetime(datetime.utcnow())
+        event = FlagEvaluationEvent(
+            repo_key=self.repository,
+            commit_sha=self.store.commit_sha,
+            feature_sha=feature_data.config_sha,
+            namespace_name=namespace,
+            feature_name=feature_data.feature.key,
+            context_keys=self.events_batcher.get_context_keys(context),
+            result_path=result.path,
+            client_event_time=timestamp,
+        )
+        self.events_batcher.add_event(event)
+
     def _get(self, namespace: str, key: str, context: Dict[str, Any]) -> ProtoAny:
         feature_data = self.store.get(namespace, key)
         result = evaluate(feature_data.feature, namespace, convert_context(context))
+        self.track(namespace, feature_data, result, context)
         return result.value
 
     ReturnType = TypeVar("ReturnType", str, float, int, bool)
