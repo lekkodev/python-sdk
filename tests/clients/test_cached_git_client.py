1b0dd6323cc13a2ccc9e42174d2affda3dfce3ad
David Kang <david@lekko.com>
2023-11-08 17:31:13 -0800

    Add mock for fs observer

--- tests/clients/test_cached_git_client.py
+++ tests/clients/test_cached_git_client.py
@@ -1,6 +1,7 @@
 from unittest import mock
 
 import pytest
+from watchdog.observers import Observer
 
 import lekko_client
 from lekko_client.clients.cached_git_client import CachedGitClient
@@ -12,13 +13,14 @@ from lekko_client.gen.lekko.backend.v1beta1.distribution_service_pb2 import (
 # Test that initialization doesn't cause errors
 def test_init():
     with mock.patch.object(CachedGitClient, "load_contents", lambda _: GetRepositoryContentsResponse()):
-        try:
-            lekko_client.initialize(
-                lekko_client.CachedGitConfig(
-                    owner_name="test_owner",
-                    repo_name="test_repo",
-                    git_repo_path="",
+        with mock.patch("watchdog.observers.Observer", spec_set=Observer):
+            try:
+                lekko_client.initialize(
+                    lekko_client.CachedGitConfig(
+                        owner_name="test_owner",
+                        repo_name="test_repo",
+                        git_repo_path="_test_/_path_",
+                    )
                 )
-            )
-        except Exception:
-            pytest.fail("cached git client initialization failed")
+            except Exception:
+                pytest.fail("cached git client initialization failed")
