6c066d6107efa4e11efeff184684c3c0d016bd03
Sergey Passichenko <sergey@lekko.com>
2023-11-22 14:58:11 -0800

    cached backend client test

--- tests/clients/test_cached_backend_client.py
+++ tests/clients/test_cached_backend_client.py
@@ -1,15 +1,29 @@
 from unittest import mock
 
 import lekko_client
+from lekko_client.gen.lekko.backend.v1beta1.distribution_service_pb2 import (
+    GetRepositoryContentsResponse,
+    GetRepositoryVersionResponse,
+)
 
 
 def test_init():
     with mock.patch("lekko_client.clients.distribution_client.DistributionServiceStub") as stub:
         stub().RegisterClient.return_value = mock.Mock(session_key="test_session_key")
-        lekko_client.initialize(
+        stub().GetRepositoryVersion.side_effect = [
+            GetRepositoryVersionResponse(commit_sha="test_commit_sha1"),
+            GetRepositoryVersionResponse(commit_sha="test_commit_sha2"),
+        ]
+        stub().GetRepositoryContents.return_value = GetRepositoryContentsResponse(
+            commit_sha="test_commit_sha1", namespaces=[]
+        )
+        client = lekko_client.initialize(
             lekko_client.CachedServerConfig(
                 owner_name="test_owner",
                 repo_name="test_repo",
                 api_key="test_api_key",
+                update_interval_ms=0,
             )
         )
+        assert client.initialized_event.wait(timeout=0.1)
+        client.close()
