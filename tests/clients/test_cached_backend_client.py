73f36202c5c8f118efa0a4ada7f0ec378c743379
David Kang <david@lekko.com>
2023-12-01 16:15:04 -0800

    Add test for should_update_store not crashing

--- tests/clients/test_cached_backend_client.py
+++ tests/clients/test_cached_backend_client.py
@@ -27,3 +27,20 @@ def test_init():
         )
         assert client.initialized_event.wait(timeout=0.1)
         client.close()
+
+
+def test_should_update_store():
+    with mock.patch("lekko_client.clients.cached_backend_client.Event") as mock_event:
+        mock_event().is_set.return_value = True
+        with mock.patch("lekko_client.clients.distribution_client.DistributionServiceStub") as mock_stub:
+            mock_stub().RegisterClient.return_value = mock.Mock(session_key="test_session_key")
+            mock_stub().GetRepositoryVersion.side_effect = Exception("test")
+            client = lekko_client.CachedBackendClient(
+                uri="test_uri",
+                store=lekko_client.MemoryStore(),
+                owner_name="test_owner",
+                repo_name="test_repo",
+                api_key="test_api_key",
+                update_interval_ms=0,
+            )
+            assert client.should_update_store() is False
